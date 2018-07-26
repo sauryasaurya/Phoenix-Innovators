997d487447ec102cfdd86bfc6284b6b4d7dc510d
sauryasaurya <saurya.bhandari78792@gmail.com>
2018-07-26 17:46:21 +0545

    Projectile added

---
+++ saurya/Projectile/projectile.js
@@ -0,0 +1,43 @@
+// const pi = 3.14159;
+// const g = 9.8;
+
+// var v = 60;
+// var angle = 45;
+// var x = 0, y = 0;
+
+// var radian = angle * pi / 180;
+// var vx = v * Math.cos(radian);
+// var vy = v * Math.sin(radian);
+
+// function xDist(vx, t){
+// 	return(vx * t);
+// }
+
+// function yDist(vy, g, t){
+// 	return((vy * t) - .5 * g * t * t);
+// }
+
+// function Range(v, g, radian){
+// 	return((2*v*v*Math.sin(radian)*Math.cos(radian)) / g);
+// }
+
+// function MaxHeight(v, g, radian){
+// 	return((v * v * Math.sin(radian) * Math.sin(radian)) / (2 * g));
+// }
+
+// x = Math.floor(xDist(vx, 6));
+// y = Math.floor(yDist(vy, g, 6));
+// R = Math.floor(Range(v, g, radian));
+// Mh = Math.floor(MaxHeight(v, g, radian));
+
+// document.write("Distance of x is " + x + "<br>");
+// document.write("Distance of y is " + y + "<br>");
+// document.write("The range of projectile is " + R + "<br>");
+// document.write("The maximum height of projectile is " + Mh + "<br>");
+
+// var FrameObj = document.getElementById('frame');
+// FrameObj.style.width = "500";
+// FrameObj.style.height = "500";
+// FrameObj.style.background = "red";
+
+// document.getElementById('frame').style.background = "red";
